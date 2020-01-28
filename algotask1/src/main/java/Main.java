a706cb666da7e4a39db41654afe658fc301ecd74
Roman Blavatskyi <roman.blavatskyi@gmail.com>
2020-01-28 07:35:10 +0200

    modified blavatskyi.roman classes

--- algotask1/src/main/java/Main.java
+++ algotask1/src/main/java/Main.java
@@ -9,16 +9,19 @@ import puiko.vadym.MathOperation;
 import pyzh.andriy.Power;
 import pyzh.andriy.SumOfTwoSquares;
 
+import blavatskyi.roman.*;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String inputChoise = args[0];
+        String inputChoice = args[0];
         int inputValue = Integer.valueOf(args[1]);
 
-        switch (inputChoise) {
+        switch (inputChoice) {
 
             //Huliaka Maksym
             case ("86a"):
@@ -42,7 +45,7 @@ public class Main {
             case ("322"):
                 System.out.println(Task.findsum());
                 break;
-                //Isachenko Dima
+            //Isachenko Dima
 
             //Latyk Borys
             case ("88в"):
@@ -80,6 +83,61 @@ public class Main {
                 break;
             // Andriy Pyzh
 
+            //Roman Blavatskyi
+            case ("178b"):
+                System.out.println("Amount of sequence members "
+                        + "by multiples of 3 and not multiples of 5: "
+                        + FirstTask.getAmountOfMultiples());
+                break;
+            case ("178c"):
+                System.out.println("Amount of sequence members "
+                        + "that are squares of even numbers: "
+                        + SecondTask.getAmountSquaresOfEvenNumbers());
+                break;
+            case ("554"):
+                ThirdTask.getAllPythagoreanTriplesOfNumber();
+                break;
+            //Roman Blavatskyi
+
         }
+
+/*        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        boolean flag = true;
+        int temp;
+        do {
+            printMenu();
+            temp = Integer.parseInt(reader.readLine());
+            if (temp == 1) {
+                System.out.println("Enter number of your task:");
+                String inputChoice = reader.readLine();
+                switch (inputChoice) {
+                    //Roman Blavatskyi
+                    case ("178b"):
+                        System.out.println("Amount of sequence members "
+                                + "by multiples of 3 and not multiples of 5: "
+                                + FirstTask.getAmountOfMultiples());
+                        break;
+                    case ("178c"):
+                        System.out.println("Amount of sequence members "
+                                + "that are squares of even numbers: "
+                                + SecondTask.getAmountSquaresOfEvenNumbers());
+                        break;
+                    case ("554"):
+                        ThirdTask.getAllPythagoreanTriplesOfNumber();
+                        break;
+                    //Roman Blavatskyi
+                }
+            } else {
+                flag = false;
+            }
+        }
+        while (flag);
+    }
+
+    private static void printMenu() {
+        System.out.println("Options menu:\n"
+                + "\tPress <- 1 -> to solve logical tasks.\n"
+                + "\tPress <- 2 -> to exit.");
+    }*/
     }
 }
