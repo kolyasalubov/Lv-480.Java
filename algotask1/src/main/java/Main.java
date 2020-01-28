8793dbd3ae37ae72e83d79002893a3d66f9e66a0
Vadym-pk <57877094+Vadym-pk@users.noreply.github.com>
2020-01-28 12:18:29 +0200

    add new lines in Main

--- algotask1/src/main/java/Main.java
+++ algotask1/src/main/java/Main.java
@@ -1,5 +1,3 @@
-package main.java;
-
 import huliaka.maksym.Calculator;
 
 import isachenko.dmytro.Task;
@@ -45,6 +43,28 @@ public class Main {
                         ThirdTask.getAllPythagoreanTriplesOfNumber();
                         break;
                     //Roman Blavatskyi
+
+                    //Puiko Vadym
+                    case ("108"):
+                        System.out.println("Дано натуральне число n. Отримати найменше число 2^r, яке перевищює n."
+                                + "\n" + "Enter n: ");
+                        System.out.println(MathOperation.getSmallestNumber(Integer.parseInt(reader.readLine())));
+                        break;
+                    case ("331a"):
+                        System.out.println("Дано натуральне число n. Можливо представити його у виді суми трьох квадратів натуальних чисел. " +
+                                "\n" + "Якщо можна, то вказати трійку x, y, z таких натуральних чисел," +
+                                "що n = x^2 + y^2 + z^2" + "\n" + "Enter n: ");
+                        MathOperation.getSumOfThreeSquaresA(Integer.parseInt(reader.readLine())).forEach(System.out::println);
+                        break;
+                    case ("331b"):
+                        System.out.println("Дано натуральне число n. Можливо представити його у виді суми трьох квадратів натуальних чисел. " +
+                                "\n" + "Якщо можна, то вказати всю трійку x, y, z таких натуральних чисел," +
+                                "що n = x^2 + y^2 + z^2" + "\n" + "Enter n: ");
+                        System.out.println(MathOperation.getSumOfThreeSquaresB(Integer.parseInt(reader.readLine())));
+                        break;
+                    //Puiko Vadym
+
+
                 }
             } else {
                 flag = false;
